19ca402acc7227093ec0191f4ed568d975e05fd7
Sam <146119225+Samalzdh@users.noreply.github.com>
2023-11-07 19:00:23 +0100

    ajout mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
+has_many :participations
+has_many :events , through: :participations
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
