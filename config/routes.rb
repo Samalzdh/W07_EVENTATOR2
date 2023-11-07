b8542f07f43c2beb97430b0cffa3bad07606a9b1
Sam <146119225+Samalzdh@users.noreply.github.com>
2023-11-07 11:40:07 +0100

    view static

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
+  get 'static_pages/secret'
   resources :events
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
