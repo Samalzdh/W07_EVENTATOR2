74ceb0f49f43872cc30f200b8d85d77f1a1c1064
Sam <146119225+Samalzdh@users.noreply.github.com>
2023-11-07 18:32:14 +0100

    modif css layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, :check_user
   def index
     
+  end
+  def check_user 
+    @user = User.find(id: params[:id])
+    if current_user.id != @user.id
+      redirect_to root_path
+    end
   end
   def show
     @user = current_user
